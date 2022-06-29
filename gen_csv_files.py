66780cde74b8ccd1d2c8be94aa339833386d5b16
manoloacademia <luispablosegovia@icloud.com>
2022-06-29 11:45:26 -0300

    Se forman los raw dataframes de las categorías.

--- gen_csv_files.py
+++ gen_csv_files.py
@@ -8,6 +8,7 @@ y las guarda en Data Frames de la librería Pandas
 import pandas as pd
 import requests
 import csv
+import numpy as np
 #from decouple import config
 
 # Columnas de información normalizadas para las fuentes
@@ -32,20 +33,18 @@ LISTA_FUENTES = [
     {'categoria': 'cines', 'url': 'https://datos.cultura.gob.ar/dataset/37305de4-3cce-4d4b-9d9a-fec3ca61d09f/resource/392ce1a8-ef11-4776-b280-6f1c7fae16ae/download/cine.csv'},
     {'categoria': 'bibliotecas', 'url': 'https://datos.cultura.gob.ar/dataset/37305de4-3cce-4d4b-9d9a-fec3ca61d09f/resource/01c6c048-dbeb-44e0-8efa-6944f73715d7/download/biblioteca_popular.csv'}
 ]
-"""
-r = requests.get(LISTA_FUENTES[1]['url'])
-download = r.content.decode('utf-8')
-csv_file = csv.reader(download.splitlines(), delimiter=',')
-csv_creator = list(csv_file)
-raw_data = pd.DataFrame(csv_creator[1:], columns=csv_creator[0])
 
-print(raw_data.head(3))
-# Realizar el download de los archivos
-
-"""
-# Definir la tabla
-#data = pd.DataFrame(columns=columnas)
-
-#LISTA_FUENTES = config('LISTA_FUENTES', cast=literal_eval)
+# Realizamos una lista en la cual se cargan los dataframes desde los .csv web
+archivos_csv = []
 for item in LISTA_FUENTES:
-    print(item['url'][:20])
+    r = requests.get(item['url'])
+    download = r.content.decode('utf-8')
+    csv_file = csv.reader(download.splitlines(), delimiter=',')
+    csv_creator = list(csv_file)
+    raw_data = pd.DataFrame(csv_creator[1:], columns=csv_creator[0])
+    archivos_csv.append(raw_data)
+    
+# Definimos los data frames para cada categoría
+df_museo = archivos_csv[0]
+df_cines = archivos_csv[1]
+df_bibliotecas = archivos_csv[2]
