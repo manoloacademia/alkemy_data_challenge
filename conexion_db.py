3e449c7819ddea12e72053a94dbcd165d93ac161
manoloacademia <luispablosegovia@icloud.com>
2022-07-08 11:31:07 -0300

    Se realizan modificaciones en los archivos de conexión a la base de datos para incluir el archivo .env y el uso de la librería decouple.

--- conexion_db.py
+++ conexion_db.py
@@ -1,6 +1,13 @@
-from sqlalchemy_conn import get_engine_from_settings
+from sqlalchemy_conn import get_engine
 import pandas as pd
 import logging
+from decouple import config
+
+user = config('DB_USER')
+passwd = config('DB_PASSWORD')
+host = config('DB_HOST')
+port = config('DB_PORT')
+db = config('DB_NAME')
 
 # Seteo de logging
 logging.basicConfig(level=logging.DEBUG, filename='datos_generados.log', filemode='a', format='%(asctime)s:%(levelname)s:%(message)s')
@@ -10,21 +17,21 @@ from gen_csv_files import gen_tabla_normalizada
 from otras_tablas import analisis_datos_cines, tabla_categoria, tabla_fuente, tabla_prov_cat
 
 # Generar tabla donde se inyectarán los valores de los data frames
-def alimentar_db(tabla: pd.DataFrame, tabla_sql: str):
+def alimentar_db(tabla: pd.DataFrame, tabla_sql: str, user, passwd, host, port, db):
     """ Esta función genera una tabla en la base de datos PostgreSQL
     y la alimenta con el data frame que se ingresa como argumento.
     """
-    engine = get_engine_from_settings()
+    engine = get_engine(user, passwd, host, port, db)
     tabla.to_sql(f'{tabla_sql}', con=engine, if_exists='replace')
 
 if __name__ == '__main__':
     
     try:
-        alimentar_db(gen_tabla_normalizada(), 'tabla_general')
-        alimentar_db(analisis_datos_cines(), 'tabla_cines')
-        alimentar_db(tabla_categoria(), 'tabla_por_categoria')
-        alimentar_db(tabla_fuente(), 'tabla_por_fuente')
-        alimentar_db(tabla_prov_cat(), 'tabla_por_provincia_categoria')
+        alimentar_db(gen_tabla_normalizada(), 'tabla_general', user, passwd, host, port, db)
+        alimentar_db(analisis_datos_cines(), 'tabla_cines', user, passwd, host, port, db)
+        alimentar_db(tabla_categoria(), 'tabla_por_categoria', user, passwd, host, port, db)
+        alimentar_db(tabla_fuente(), 'tabla_por_fuente', user, passwd, host, port, db)
+        alimentar_db(tabla_prov_cat(), 'tabla_por_provincia_categoria', user, passwd, host, port, db)
         logging.info('Se crean las tablas correspondientes en la base de datos.')
-    except:
-        logging.warning('Revisar los datos de conexión.')
+    except Exception as e:
+        logging.warning(f'Revisar los datos de conexión: {e}.')
--- sqlalchemy_conn.py
+++ sqlalchemy_conn.py
@@ -3,9 +3,7 @@
 2- We need to create a session
 """
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
 from sqlalchemy_utils import database_exists, create_database
-from local_settings import postgresql as settings
 
 # Se realiza la función que va a crear el motor con SQL Alchemy
 def get_engine(user, passwd, host, port, db):
@@ -14,29 +12,3 @@ def get_engine(user, passwd, host, port, db):
         create_database(url)
     engine = create_engine(url, pool_size=50, echo=False)
     return engine
-
-# Se prueba para ver si está creado
-# print(engine.url) --> arroja un resultado correcto
-
-# Se crea una función que tome todos los parámetros de settings y cree en engine para la sesión
-def get_engine_from_settings():
-    keys = ['pguser', 'pgpasswd', 'pghost', 'pgport', 'pgdb']
-    if not all(key in keys for key in settings.keys()):
-        raise Exception('Corregir archivo de configuración.')
-    
-    return get_engine(settings['pguser'],
-                    settings['pgpasswd'],
-                    settings['pghost'],
-                    settings['pgport'],
-                    settings['pgdb'])
-
-# Se crea sesión
-def get_session():
-    engine = get_engine_from_settings()
-    session = sessionmaker(bind=engine) ()
-    return session
-
-session = get_session()
-
-# Se prueba para corroborar si funciona la sesión
-# print(session) --> funciona correctamente
